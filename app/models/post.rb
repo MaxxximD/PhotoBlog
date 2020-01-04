1a9de09c9fc790297e44ba7e0527c40a3c37f952
MaxxximD <dyachenko.maxim@bk.ru>
2020-01-05 01:16:00 +0300

    changed upload of images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,11 @@
 class Post < ApplicationRecord
   has_rich_text :body
-  mount_uploader :image, ImageUploader
+  #mount_uploader :image, ImageUploader
   has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
   belongs_to :category
+
+  has_one_attached :image
 
   validates :title, :summary, :body, presence: true
 
@@ -16,7 +18,14 @@ class Post < ApplicationRecord
       Tag.where(name: name.strip).first_or_create!
     end
   end
-  
-  has_one_attached :image
+
+
+  def thumb
+    return self.image.variant(resize: '250x250').processed
+  end
+
+  def large
+    return self.image.variant(resize: '800x800').processed
+  end
 
 end
